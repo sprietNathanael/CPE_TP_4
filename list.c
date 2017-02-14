905afe1db7246ae5562763d724cb029b2e12f079
Nathanaël Spriet <nathanael.spriet@gmail.com>
2017-02-14 08:04:37 +0100

    Adds an empty librairie to manage chained list

---
+++ list.c
@@ -0,0 +1,8 @@
+/**
+ * CPE-TP4 : Implémentation d'une liste dans une liste chaînée
+ *
+ * @file list.c
+ * @brief      Useful tools to manage chained list
+ * @author     Nathanaël SPRIET
+ * @date       14/02/2017
+ */
---
+++ list.h
@@ -0,0 +1,16 @@
+/**
+ * CPE-TP4 : Implémentation d'une liste dans une liste chaînée
+ *
+ * @file list.h
+ * @brief      Useful tools to manage chained list
+ * @author     Nathanaël SPRIET
+ * @date       14/02/2017
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef FILE_H
+#define FILE_H
+
+#endif
